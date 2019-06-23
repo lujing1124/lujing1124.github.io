efe677ea5370410cf82ae9f6c90e74ab96ef3957
lujing <lj_1124@126.com>
2019-06-23 16:36:14 +0800

    加了一个注释

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 /*
- * @Author: Administrator
+ * @Author: jenny
  * @Date:   2019-06-03 01:25:52
  * @Last Modified by:   Administrator
  * @Last Modified time: 2019-06-11 22:06:12
@@ -11,18 +11,20 @@
         this.food = new Food();
         this.snake = new Snake();
         this.map = map;
-        //this.speed = speed;
+        this.speed = 1000;
         that = this;
     }
 
     Game.prototype.init = function () {
         this.food.init(this.map);
         this.snake.init(this.map);
-        this.snakeRun(this.map, this.food);
+
+        this.snakeRun(this.map, this.food, this.speed);
         this.bindKey();
     };
 
-    Game.prototype.snakeRun = function (map, food) {
+    Game.prototype.snakeRun = function (map, food, speed) {
+        //即使速度传参数，但是有改变的时候，定时器不识别
         var timeId = setInterval(function () {
             this.snake.move(map, food);
             this.snake.init(map);
@@ -48,7 +50,7 @@
             /*if(headX == food.x && headY == food.y){
              alert('get');
              }*/
-        }.bind(that), 100);
+        }.bind(that),speed);
     };
 
     Game.prototype.bindKey = function () {
@@ -96,8 +98,34 @@
             }else if(e.keyCode == 40){
                 this.snake.direction = 'bottom';
             }
+            //控制蛇跑的速度，每次换个速度，就会导致多一条蛇，然后越跑越快，哪怕换回慢的速度，也慢不下来
+            if(e.keyCode == 49 || e.keyCode == 97){
+                this.speed = 1000;
+                console.log(this.speed);
+                //this.snakeRun(this.map, this.food, this.speed);
+            }else if(e.keyCode == 50 || e.keyCode == 98){
+                this.speed = 500;
+                console.log(this.speed);
+                //this.snakeRun(this.map, this.food, this.speed);
+            }else if(e.keyCode == 51 || e.keyCode == 99){
+                this.speed = 300;
+                console.log(this.speed);
+                //this.snakeRun(this.map, this.food, this.speed);
+            }else if(e.keyCode == 52 || e.keyCode == 100){
+                this.speed = 200;
+                console.log(this.speed);
+                //this.snakeRun(this.map, this.food, this.speed);
+            }else if(e.keyCode == 53 || e.keyCode == 101){
+                this.speed = 100;
+                console.log(this.speed);
+                // this.snakeRun(this.map, this.food, this.speed);
+            }
 
         }.bind(that),false);
+
+        /*document.getElementById('speed').addEventListener('onchange',function () {
+            console.log(11);
+        },false);*/
 
     }
 
